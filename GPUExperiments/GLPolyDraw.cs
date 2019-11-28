29994d6a2f003a5ac5d00a35101ce7666eb1ab09
Robin Debreuil <it3rate@gmail.com>
2019-11-28 15:45:54 -0600

    Drawing vertexes from the compute shader.

--- GPUExperiments/GLPolyDraw.cs
+++ GPUExperiments/GLPolyDraw.cs
@@ -11,6 +11,23 @@ using OpenTK.Graphics.OpenGL4;
 
 namespace GPUExperiments
 {
+	struct DrawArraysIndirectCommand
+	{
+		public uint Count;
+		public uint PrimCount;
+		public uint First;
+		public uint ReservedMustBeZero;
+		public DrawArraysIndirectCommand(uint count, uint primCount, uint first = 0)
+		{
+			Count = count;
+			PrimCount = primCount;
+			First = first;
+			ReservedMustBeZero = 0;
+		}
+
+		public IntPtr Size => (IntPtr)(4 * sizeof(uint));
+    }
+
     public class GLPolyDraw
     {
 	    private int _textureId;
@@ -46,22 +63,31 @@ namespace GPUExperiments
 		        +0.0f, +0.5f
 	        };
 
-	        _vbo = GL.GenBuffer();
-	        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            _vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
             GL.BufferData(BufferTarget.ArrayBuffer, data.Length * sizeof(float), data, BufferUsageHint.StaticDraw);
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 
-	        _vba = GL.GenVertexArray();
-	        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            _vba = GL.GenVertexArray();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
             GL.BindVertexArray(_vba);
             GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 0, 0);
-	        GL.EnableVertexAttribArray(0);
-	        GL.BindVertexArray(0);
+            GL.EnableVertexAttribArray(0);
+            GL.BindVertexArray(0);
+
+	        _indirect = GL.GenBuffer();
+	        GL.BindBuffer(BufferTarget.DrawIndirectBuffer, _indirect);
+	        var pts = new DrawArraysIndirectCommand(3, 1);
+            GL.BufferData(BufferTarget.DrawIndirectBuffer, pts.Size, ref pts, BufferUsageHint.StaticDraw);
+            //GL.BindBuffer(BufferTarget.DrawIndirectBuffer, 0);
+
+            GL.BindBufferBase(BufferRangeTarget.ShaderStorageBuffer, 1, _vbo); // Buffer Binding 1
 
             _loaded = true;
 	        _gl.Invalidate();
         }
 
+        private int _indirect;
         private int _vbo;
         private int _vba;
         private void glControl1_Paint(object sender, PaintEventArgs e)
@@ -69,13 +95,13 @@ namespace GPUExperiments
 	        if (!_loaded) return;
 	        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-	        //_computeShader.Use();
-	        //GL.DispatchCompute(256, 256, 1);
+	        _computeShader.Use();
+	        GL.DispatchCompute(256, 256, 1);
 
 	        _vfShader.Use();
 	        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
 	        GL.BindVertexArray(_vba);
-            GL.DrawArrays(PrimitiveType.Triangles, 0,  3);
+            GL.DrawArraysIndirect(PrimitiveType.Triangles, IntPtr.Zero);
 
 	        _gl.SwapBuffers();
         }
