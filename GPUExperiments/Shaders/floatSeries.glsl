e26a036cc07895c06f64e54f38d2b165270e8b7a
Robin Debreuil <it3rate@gmail.com>
2019-12-01 12:50:50 -0600

    Adjusted return sized in float Series.

--- GPUExperiments/Shaders/floatSeries.glsl
+++ GPUExperiments/Shaders/floatSeries.glsl
@@ -6,35 +6,35 @@ layout(std430, binding = 3) buffer FloatSeriesBuffer {
 };
 
 
-vec4 floatAt(uint seriesIndex, uint index)
+float floatAt(uint seriesIndex, uint index)
 {
 	int seriesAddress = floatSeriesOffset[seriesIndex];
 	return floatSeriesOffset[seriesAddress + index];
 }
-vec4 vec2At(uint seriesIndex, uint index)
+vec2 vec2At(uint seriesIndex, uint index)
 {
 	int seriesAddress = floatSeriesOffset[seriesIndex];
 	int startAddress = seriesAddress + index * 2;
-	return vec4(floatSeriesOffset[
-		startAddress + 0],
+	return vec2(
+		floatSeriesOffset[startAddress + 0],
 		floatSeriesOffset[startAddress + 1]);
 }
-vec4 vec3At(uint seriesIndex, uint index)
+vec3 vec3At(uint seriesIndex, uint index)
 {
 	int seriesAddress = floatSeriesOffset[seriesIndex];
 	int startAddress = seriesAddress + index * 3;
-	return vec4(floatSeriesOffset[
-		startAddress + 0],
+	return vec3(
+		floatSeriesOffset[startAddress + 0],
 		floatSeriesOffset[startAddress + 1],
-			floatSeriesOffset[startAddress + 2]);
+		floatSeriesOffset[startAddress + 2]);
 }
 vec4 vec4At(uint seriesIndex, uint index)
 {
 	int seriesAddress = floatSeriesOffset[seriesIndex];
 	int startAddress = seriesAddress + index * 4;
-	return vec4(floatSeriesOffset[
-		startAddress + 0],
+	return vec4(
+		floatSeriesOffset[startAddress + 0],
 		floatSeriesOffset[startAddress + 1],
-			floatSeriesOffset[startAddress + 2],
-			floatSeriesOffset[startAddress + 3]);
+		floatSeriesOffset[startAddress + 2],
+		floatSeriesOffset[startAddress + 3]);
 }
